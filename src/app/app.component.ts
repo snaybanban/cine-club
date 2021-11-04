38374859b391911fd03213304350c1812e2babb7
snaybanban <angelosanches100@gmail.com>
2021-11-03 20:41:05 -0600

    iconos de menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Cartelera', url: '/home/interfas/Inbox', icon: 'mail' },
-    { title: 'Estrenos', url: '/home/interfas/Outbox', icon: 'paper-plane' },
+    { title: 'Cartelera', url: '/home/interfas/Inbox', icon: 'today' },
+    { title: 'Estrenos', url: '/home/interfas/Outbox', icon: 'film' },
     { title: 'Claro Video', url: '/home/interfas/Favorites', icon: 'heart' },
-    { title: 'Descargas', url: '/home/interfas/Archived', icon: 'archive' },
-    { title: 'Portadas', url: '/home/interfas/Trash', icon: 'trash' },
+    { title: 'Descargas', url: '/home/interfas/Archived', icon: 'download' },
+    { title: 'Portada', url: '/home/interfas/Trash', icon: 'home' },
     { title: 'Ayuda', url: '/home/interfas/Spam', icon: 'warning' },
-    { title: 'Salir', url: '/home/interfas/Spam', icon: 'warning' },
+    { title: 'Salir', url: '/home/interfas/Spam', icon: 'person-circle' },
   ];
   constructor() {}
 }
